50ad408caf889bf010a2320a2720caee0c4ab188
amigin <amigin@gmail.com>
2025-04-03 22:11:31 +0300

    Namespace is removed

--- src/socket_io_settings.rs
+++ src/socket_io_settings.rs
@@ -2,7 +2,6 @@
 pub trait SocketIoClientSettings {
     async fn get_server_url(&self, client_name: &str) -> String;
     async fn get_handshake_path(&self, client_name: &str) -> String;
-    async fn get_namespace(&self, client_name: &str) -> String;
     async fn get_headers(&self, client_name: &str) -> Vec<(String, String)>;
     async fn get_query_params(&self, client_name: &str) -> Vec<(String, String)>;
 }
